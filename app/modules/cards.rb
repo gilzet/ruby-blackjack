a8b6a43e58c21ca778b24f7d2bde3bf79402d478
Gilzet <31349186+Gilzet@users.noreply.github.com>
2024-08-12 00:23:35 +0300

    Add cards methods

--- app/modules/cards.rb
+++ app/modules/cards.rb
@@ -1,4 +1,30 @@
 # frozen_string_literal: true
 
 module Cards
+  def get_value(cards)
+    values = cards.map { |card| CARDS[card][:value] }
+    if values.include?(nil)
+      values.sort_by! { |value| value.nil?.to_s } # put nils to end of array
+      sum = 0
+      values.each { |value| sum += value || (sum += 11 <= BLACK_JACK ? 11 : 1) }
+      sum
+    else
+      values.sum
+    end
+  end
+
+  def get_pics(cards)
+    pics = cards.map { |card| CARDS[card][:pic] }
+    pics.join(', ')
+  end
+
+  def arbiter(first_value, second_value)
+    if first_value == second_value || (first_value > BLACK_JACK && second_value > BLACK_JACK)
+      :draw
+    elsif (first_value > second_value && first_value <= BLACK_JACK) || second_value > BLACK_JACK
+      :win
+    else
+      :lose
+    end
+  end
 end
