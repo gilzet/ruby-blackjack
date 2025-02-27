617694aa910ab5e496d5ebce5fd105be7767d74c
Gilzet <31349186+Gilzet@users.noreply.github.com>
2025-02-27 10:12:23 +0300

    fix relatives

--- main.rb
+++ main.rb
@@ -2,7 +2,6 @@
 
 require_relative 'app/modules/constants'
 require_relative 'app/modules/cards'
-require_relative 'app/modules/validation'
 require_relative 'app/classes/game'
 require_relative 'app/classes/player'
 require_relative 'app/classes/user'
